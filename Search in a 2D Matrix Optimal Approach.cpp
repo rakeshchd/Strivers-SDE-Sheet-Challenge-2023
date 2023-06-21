6ff75f41a29497a15b13ef4f9a2e2205bec859e1
Rakesh Choudhary <120676797+rakeshchd@users.noreply.github.com>
2023-06-21 23:40:54 +0530

    Create Search in a 2D Matrix Optimal Approach.cpp

---
+++ Search in a 2D Matrix Optimal Approach.cpp
@@ -0,0 +1,13 @@
+bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int low = 0;
+        int high = m*n-1;
+        while(low <= high){
+            int mid = (high+low)/2;
+            if(mat[mid/m][mid%m] == target) return true;
+            else if(mat[mid/m][mid%m] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return false;
+}
